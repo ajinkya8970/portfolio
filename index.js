02ed8d92e8e18bb9969dc671e10a96fdc30ca096
Ajinkya Teltumade <105469018+ajinkya8970@users.noreply.github.com>
2024-01-09 10:13:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let target = 10;
+
+if (target == 1) {
+  console.log("target is 1");
+} else if (target == 2) {
+  console.log("target is 2");
+} else if (target == 10) {
+  console.log("target is 10");
+} else {
+  console.log("notmatched");
+}
